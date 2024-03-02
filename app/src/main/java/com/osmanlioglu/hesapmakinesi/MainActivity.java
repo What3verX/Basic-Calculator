d7802017e965ed2a38a41ace98929fbd4d7d0017
firstryer <huseyinosmanlioglu@outlook.com>
2024-03-02 20:28:43 +0300

    Layout Update

--- app/src/main/java/com/osmanlioglu/hesapmakinesi/MainActivity.java
+++ app/src/main/java/com/osmanlioglu/hesapmakinesi/MainActivity.java
@@ -2,77 +2,133 @@ package com.osmanlioglu.hesapmakinesi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    EditText Number1Text; //Number1Text isminde EditText turunden bir degisken yarattim.
+    EditText Number1Text;
     EditText Number2Text;
     TextView ResultText;
+    Handler handler;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Number1Text = findViewById(R.id.Number1Text); //Yarattigim degiskeninin gorsel kisimda olusturdugum EditText oldugunu bildirdim.
+        Number1Text = findViewById(R.id.Number1Text);
         Number2Text = findViewById(R.id.Number2Text);
         ResultText = findViewById(R.id.ResultText);
-    }
-    public void topla(View view){
-        if(Number1Text.getText().toString().matches("") || Number2Text.getText().toString().matches("")){
-            ResultText.setText("Enter Number!");
-        }
+        handler = new Handler();
 
-        else {
-            int number1 = Integer.parseInt(Number1Text.getText().toString()); //Number1Text degiskenimin tuttugu texti alip stringe cevirip onu da integera cevirerek number1 adinda bir degiskene atadim.
-            //number1 degiskenini goruntuledigimde artik Number1Text'e girilen sayiyi goruntuleyebilecegim.
-
-            int number2 = Integer.parseInt(Number2Text.getText().toString());
-            int result = number1 + number2;
-            ResultText.setText("Result: " + result);
-        }
     }
 
-    public void cikar(View view){
+
+
+
+    public void sum(View view){
         if(Number1Text.getText().toString().matches("") || Number2Text.getText().toString().matches("")){
             ResultText.setText("Enter Number!");
         }
         else {
-            int number1 = Integer.parseInt(Number1Text.getText().toString());
+            Button clickedButton = (Button) view;
+            ColorStateList colorStateList = ColorStateList.valueOf(Color.RED);
+            clickedButton.setBackgroundTintList(colorStateList);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ColorStateList colorStateList = ColorStateList.valueOf(Color.BLUE);
+                    clickedButton.setBackgroundTintList(colorStateList);
+                }
+            }, 500);
 
-            int number2 = Integer.parseInt(Number2Text.getText().toString());
-            int result = number1 - number2;
+
+            float number1 = Integer.parseInt(Number1Text.getText().toString());
+
+            float number2 = Integer.parseInt(Number2Text.getText().toString());
+            float result = number1 + number2;
             ResultText.setText("Result: " + result);
         }
-    }
+        }
 
-    public void Carp(View view){
+
+    public void sub(View view){
         if(Number1Text.getText().toString().matches("") || Number2Text.getText().toString().matches("")){
             ResultText.setText("Enter Number!");
         }
         else {
-            int number1 = Integer.parseInt(Number1Text.getText().toString());
+            Button clickedButton = (Button) view;
+            ColorStateList colorStateList = ColorStateList.valueOf(Color.RED);
+            clickedButton.setBackgroundTintList(colorStateList);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ColorStateList colorStateList = ColorStateList.valueOf(Color.BLUE);
+                    clickedButton.setBackgroundTintList(colorStateList);
+                }
+            }, 500);
 
-            int number2 = Integer.parseInt(Number2Text.getText().toString());
-            int result = number1 * number2;
+            float number1 = Integer.parseInt(Number1Text.getText().toString());
+
+            float number2 = Integer.parseInt(Number2Text.getText().toString());
+            float result = number1 - number2;
             ResultText.setText("Result: " + result);
         }
     }
 
-    public void  Bol(View view){
+    public void multiplication(View view){
+        if(Number1Text.getText().toString().matches("") || Number2Text.getText().toString().matches("")){
+            ResultText.setText("Enter Number!");
+        }
+        else {
+            Button clickedButton = (Button) view;
+            ColorStateList colorStateList = ColorStateList.valueOf(Color.RED);
+            clickedButton.setBackgroundTintList(colorStateList);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ColorStateList colorStateList = ColorStateList.valueOf(Color.BLUE);
+                    clickedButton.setBackgroundTintList(colorStateList);
+                }
+            }, 500);
+            float number1 = Integer.parseInt(Number1Text.getText().toString());
+
+            float number2 = Integer.parseInt(Number2Text.getText().toString());
+            float result = number1 * number2;
+            ResultText.setText("Result: " + result);
+        }
+    }
+
+    public void  division(View view){
         if(Number1Text.getText().toString().matches("")|| Number2Text.getText().toString().matches("")){
             ResultText.setText("Enter Number!");
         }
         else if (Number2Text.getText().toString().matches("0")){
-            ResultText.setText("I don't know bro! wtf?");
+            ResultText.setText("Error");
         }
         else {
-            int number1 = Integer.parseInt(Number1Text.getText().toString());
+            Button clickedButton = (Button) view;
+            ColorStateList colorStateList = ColorStateList.valueOf(Color.RED);
+            clickedButton.setBackgroundTintList(colorStateList);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    ColorStateList colorStateList = ColorStateList.valueOf(Color.BLUE);
+                    clickedButton.setBackgroundTintList(colorStateList);
+                }
+            }, 500);
+            float number1 = Integer.parseInt(Number1Text.getText().toString());
 
-            int number2 = Integer.parseInt(Number2Text.getText().toString());
-            int result = number1 / number2;
+            float number2 = Integer.parseInt(Number2Text.getText().toString());
+            float result = number1 / number2;
             ResultText.setText("Result: " + result);
         }
     }
+
+
 }
